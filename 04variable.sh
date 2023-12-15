9c2f54f2f7c0de398b8f907f3a10d35210b52e45
lakshmibandla <lakshmi4info@gmail.com>
2023-12-15 13:47:57 +0530

    hello ramesh

---
+++ 04variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo"$PERSON1: hello $PERSON2, GUDMNG"
+echo "suresh: hi ramesh, very gudmng"
+echo "ramesh: how r u suresh?"
+echo "suresh: Iam gud ,How R u"
