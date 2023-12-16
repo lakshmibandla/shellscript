0efb6a94f76022a036bba2c5e7196f3df4463d7c
lakshmibandla <lakshmi4info@gmail.com>
2023-12-16 12:33:39 +0530

    learning

--- 07condition.sh
+++ 07condition.sh
@@ -2,7 +2,7 @@
 NUMBER=$1
 if [ $number -gt 100 ]
 then
-eche "give the number $number is gretaer than 100"
+echo "give the number $number is gretaer than 100"
 else 
 echo "give the number $number is not greaterthan 100"
 fi
