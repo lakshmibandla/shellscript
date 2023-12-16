5fc665c8588f9bdec39fc62aefd7e57206c9b4f9
lakshmibandla <lakshmi4info@gmail.com>
2023-12-16 13:41:14 +0530

    improving

--- installmysql.sh
+++ installmysql.sh
@@ -3,7 +3,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
 echo "ERROR:: please run this script with root access"
+exit 1
 else
 echo "you are the root user"
 fi
 yum install mysql -y
+
+if [$? -ne 0 ]
+then
+echo "ERROR::installing my sql faield"
+exit 1
+else
+echo "installing my sql is success"
+fi
