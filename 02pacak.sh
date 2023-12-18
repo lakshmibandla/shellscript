49e7136f3f2b2649f0f59ec3fa7795702dd06e04
lakshmibandla <lakshmi4info@gmail.com>
2023-12-18 13:11:19 +0530

    changes

--- 02pacak.sh
+++ 02pacak.sh
@@ -3,6 +3,14 @@ ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then 
+    echo -e "$2...$R FAIL $N"
+    else 
+    echo -e "$2...$G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
 then 
@@ -12,4 +20,17 @@ else
 echo "you are the root user"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+#git mysql postfix nettools
+#pacakages=git for the first time
+
+
+
+for package in $@ 
+do
+yum list installed $package
+if [ $? -ne 0 ]
+then
+yum install $package -y
+VALIDATE $?
+done
