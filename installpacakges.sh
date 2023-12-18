0a5c80f31b1030c47c33a37f348fe2a2f02c3731
lakshmibandla <lakshmi4info@gmail.com>
2023-12-18 12:19:38 +0530

    changes

--- installpacakges.sh
+++ installpacakges.sh
@@ -9,4 +9,20 @@ else
 echo "you are the root user"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+#"git my sql test-tool"
+#pacakage=for the first time
+
+for pacakage in $@
+do
+yum list installed $pacakages
+if [$? -ne 0 ]
+then 
+yum install $pacakages -y
+VALIDATE $?
+fi
+
+
+
+
+done
