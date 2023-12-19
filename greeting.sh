780e6162a05640f4cc94712ef0e8dfb8086a8d24
lakshmibandla <lakshmi4info@gmail.com>
2023-12-19 13:52:24 +0530

    changes

--- greeting.sh
+++ greeting.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-PERSON=$1
-echo "Hello $PERSON, Gudmng.iam learning shell"
+NAME=""
+WISHES=""
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "options::"
+    echo " -n, specify the name (mandatory)"
+    echo " -w, specify the wishes ex gud mng"
+    echo " -h, Display Help And Exit"
+}
+while getopts ":n:w:h" opt; do
+case $opt in
+    n) NAME="$OPTARG";;
+    w) WISHES="$OPTARG";;
+    h|* USAGE; exit;;      
+    esac
+done
+
+
+
+
+
+
